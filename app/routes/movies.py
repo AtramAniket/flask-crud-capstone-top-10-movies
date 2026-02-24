446ad4d25497198efd1dbe94f01d635ce64cf90d
AtramAniket <aniket_atram@live.in>
2026-02-24 16:25:07 +0530

    finished CRUD

--- app/routes/movies.py
+++ app/routes/movies.py
@@ -30,6 +30,29 @@ def add_new_movie():
 	return render_template('movie_form.html', form=form, page_title="Add New Movie", form_heading = "Add New Movie")
 
 
-@movies_bp.route('/edit/<int:movie_id>')
+@movies_bp.route('/edit/<int:movie_id>', methods=['GET', 'POST'])
 def edit_movie(movie_id):
-	return render_template('edit.html')
+
+	movie = Movie.query.get_or_404(movie_id)
+	
+	form = MovieForm(obj=movie)
+
+	form.submit.label.text = 'Update'
+
+	if form.validate_on_submit():
+		form.populate_obj(movie)
+		db.session.add(movie)
+		db.session.commit()
+		return redirect(url_for('movies.home'))
+	return render_template('movie_form.html', form=form, page_title="Edit Movie", form_heading = "Edit Movie")
+
+@movies_bp.route('/delete/<int:movie_id>', methods=['GET','POST'])
+def delete_movie(movie_id):
+	
+	movie = Movie.query.get_or_404(movie_id)
+	
+	db.session.delete(movie)
+	db.session.commit()
+
+	return redirect(url_for('movies.home'))
+		
